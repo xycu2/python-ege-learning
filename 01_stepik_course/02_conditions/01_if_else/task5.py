e8efee94e29e9b8789653102144d05bb732b3da6
xycu <smonindemid@gmail.com>
2026-08-05 14:46:20 +0500

    feat: task5 if else

---
+++ 01_stepik_course/02_conditions/01_if_else/task5.py
@@ -0,0 +1,17 @@
+count = 0
+
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+if num1 % 2 == 0:
+    count += 1
+
+if num2 % 2 == 0:
+    count += 1
+
+if num3 % 2 == 0:
+    count += 1
+
+
+print(count)
