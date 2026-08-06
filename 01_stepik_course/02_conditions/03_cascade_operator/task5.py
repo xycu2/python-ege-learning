73c7e8f4ea4c346519d9d7c71c18d7c3193bb68e
xycu <smonindemid@gmail.com>
2026-08-06 22:03:09 +0500

    feat: task5 cascade operator

---
+++ 01_stepik_course/02_conditions/03_cascade_operator/task5.py
@@ -0,0 +1,9 @@
+dress1 = input()
+dress2 = input()
+
+if dress1 != 'Розовое' and dress2 != 'Розовое':
+    print('Белые')
+elif dress1 != 'Красное' and dress2 != 'Красное':
+    print('Черные')
+else:
+    print('Голубые')
